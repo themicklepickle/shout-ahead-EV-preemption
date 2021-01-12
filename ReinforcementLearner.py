243ce8ec5aeaccd5820462ab61cd2d89780945a3
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-01-11 23:10:34 -0700

    Added EV parameters to ReinforcementLearner.py - added weight factor for change in EV speed and change in EV traffic density - added temporary penalty value for a stopped EV - changed updatedWeight() to include new parameters - changed determineReward() to include EVChangeInSpeed and EVChangeInTrafficDensity - changed determinePenalty() to include EVIsStopped

--- ReinforcementLearner.py
+++ ReinforcementLearner.py
@@ -7,28 +7,38 @@ global learningFactor           # Influences rate with which the weight value co
 global discountRate             # Determines the emphasis on the importance of future evaluations
 global throughputFactor         # Determines the emphasis throughput has on the size of the reward
 global waitTimeReducedFactor    # Determines the emphasis reduced waiting time has on the size of the reward
+global EVSpeedFactor
+global EVTrafficDensityFactor
 global penaltyMultiplier        # The penalty multiplier attached to the number of excess cars at an intersection after a rule is applied compared to before; assigned to a rule for poor performance
+global EVIsStoppedPenalty
 learningFactor = 0.5
 discountRate = 0.5
 throughputFactor = 1
 waitTimeReducedFactor = 1
+EVSpeedFactor = 1
+EVTrafficDensityFactor = 1
 penaltyMultiplier = -0.05
+EVIsStoppedPenalty = -100
 
 
-def updatedWeight(rule, nextRule, throughputRatio, waitTimeReducedRatio, intersectionQueueDifference):
+def updatedWeight(rule, nextRule, throughputRatio, waitTimeReducedRatio, intersectionQueueDifference, EVChangeInSpeed, EVChangeInTrafficDensity, EVIsStopped):
     # Returns the updated weight based on the Sarsa learning method
-    updatedWeight = rule.getWeight() + (learningFactor*(determineReward(throughputRatio, waitTimeReducedRatio) + (discountRate*nextRule.getWeight() - rule.getWeight()))) + determinePenalty(intersectionQueueDifference)
+    updatedWeight = rule.getWeight() + (learningFactor*(determineReward(throughputRatio, waitTimeReducedRatio, EVChangeInSpeed, EVChangeInTrafficDensity) +
+                                                        (discountRate*nextRule.getWeight() - rule.getWeight()))) + determinePenalty(intersectionQueueDifference, EVIsStopped)
 
     return updatedWeight * 0.0001  # Numbers are reduced by 99.99% to keep them managable
 
 
 # Function to determine the reward
-def determineReward(throughputRatio, waitTimeReducedRatio):
-    return (throughputFactor*throughputRatio) + (waitTimeReducedFactor*waitTimeReducedRatio)
+def determineReward(throughputRatio, waitTimeReducedRatio, EVChangeInSpeed, EVChangeInTrafficDensity):
+    return (throughputFactor * throughputRatio) + (waitTimeReducedFactor * waitTimeReducedRatio) + \
+        (EVSpeedFactor * EVChangeInSpeed) + (EVTrafficDensityFactor * EVChangeInTrafficDensity)
 
 
-def determinePenalty(intersectionQueueDifference):
+def determinePenalty(intersectionQueueDifference, EVIsStopped):
+    penalty = 0
     if intersectionQueueDifference > 0:
-        return intersectionQueueDifference*penaltyMultiplier
-    else:
-        return 0
+        penalty += intersectionQueueDifference * penaltyMultiplier
+    if EVIsStopped is True:
+        penalty += EVIsStoppedPenalty
+    return penalty
