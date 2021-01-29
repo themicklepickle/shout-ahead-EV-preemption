d2f2015795785a5a5d684747580a32d4e9425d12
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-01-29 09:35:57 -0700

    Fixed file formatting in OutputManager.py

--- OutputManager.py
+++ OutputManager.py
@@ -25,40 +25,25 @@ def run(agentPools, avgGenTime, totalGenTime, generations, totalGenerations, fol
     f.write("Best Individuals per Agent Pool\n")
 
     for ap in agentPools:
-        actionSet = ""
-        for a in ap.getActionSet()[:-1]:
-            actionSet += f"{a}, "
-        actionSet += ap.getActionSet()[-1]
-
         f.write(f"Agent Pool {ap.getID()}\n")
-        f.write(f"This agent pool has an action set of: {actionSet}\n")
+        f.write(f"This agent pool has an action set of: {', '.join(ap.getActionSet())}\n")
 
         individuals = ap.getIndividualsSet()
         individuals.sort
         topIndividual = min(individuals, key=attrgetter('fitness'))
-        f.write(f"The top individual has a fitness of {topIndividual.getFitness()} and its RS and RSint sets contain the following rules (formatted as \"<conditions>, <action>\"):\n\n")
+        f.write(f"The top individual has a fitness of {topIndividual.getFitness()}\n")
 
         f.write("RS:\n")
-        ruleCount = 1
         for rule in topIndividual.getRS():
-            cond = ""
-            for c in rule.getConditions()[:-1]:
-                cond += f"{c}, "
-            cond += rule.getConditions()[-1]
-
-            f.write(f"RS Rule {ruleCount}: <{cond}>, <{rule.getAction()}> and rule has a weight of {rule.getWeight()}\n\n")
-            ruleCount += 1
+            f.write(rule)
 
         f.write("RSint:\n")
-        ruleCount = 1
         for rule in topIndividual.getRSint():
-            cond = ""
-            for c in rule.getConditions()[:-1]:
-                cond += f"{c}, "
-            cond += rule.getConditions()[-1]
+            f.write(rule)
 
-            f.write(f"RSint Rule {ruleCount}: <{cond}>, <{rule.getAction()}> and rule has a weight of {rule.getWeight()}\n\n")
-            ruleCount += 1
+        f.write("RSev:\n")
+        for rule in topIndividual.getRSev():
+            f.write(rule)
 
         f.write("*******\n")
 
