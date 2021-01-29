397f95dd8e5d3888301b89c6b3cc02ff8313adf3
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-01-29 13:56:00 -0700

    Fixed an error

--- CoopPredicateSet.py
+++ CoopPredicateSet.py
@@ -277,7 +277,7 @@ def getPredicateSet(agentPool):
 def getPredicateSetFromFile(file):
     predicateList = []
     f = open(file, "r")  # Open desired file
-    for _ in f:
+    for x in f:
         if "//" in x:
             continue
         elif x.strip() == "":
