f0acf3e51de0b93a9a484e78739165b5d48231dc
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-01-31 12:52:49 -0700

    Cleaned up CoopPredicateSet - minimized predicates - added type checking - removed unnneccessary imports and functions

--- CoopPredicateSet.py
+++ CoopPredicateSet.py
@@ -1,270 +1,63 @@
-import os
+from __future__ import annotations
+
 import sys
-import optparse
 import inspect
 from random import randrange
 
-#------------------------ intendedAction predicates [DEPRECATED] -------------------------#
-# def intendedActionIs_H_S_G(action):
-#     if action == "H_S_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_H_S_Y(action):
-#     if action == "H_S_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_H_L_G(action):
-#     if action == "H_L_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_H_L_Y(action):
-#     if action == "H_L_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_V_S_G(action):
-#     if action == "V_S_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_V_S_Y(action):
-#     if action == "V_S_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_V_L_G(action):
-#     if action == "V_L_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_V_L_Y(action):
-#     if action == "V_L_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_NS_S_G(action):
-#     if action == "NS_S_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_NS_S_Y(action):
-#     if action == "NS_S_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_NS_SL_G(action):
-#     if action == "NS_S_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_NS_SL_Y(action):
-#     if action == "NS_S_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_NS_L_G(action):
-#     if action == "NS_L_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_NS_L_Y(action):
-#     if action == "NS_L_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_SN_S_G(action):
-#     if action == "SN_S_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_SN_S_Y(action):
-#     if action == "SN_S_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_SN_SL_G(action):
-#     if action == "SN_SL_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_SN_S_Y(action):
-#     if action == "SN_SL_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_SN_L_G(action):
-#     if action == "SN_L_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_SN_L_Y(action):
-#     if action == "SN_L_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_EW_S_G(action):
-#     if action == "EW_S_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_EW_S_Y(action):
-#     if action == "EW_S_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_EW_SL_G(action):
-#     if action == "EW_S_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_EW_SL_Y(action):
-#     if action == "EW_S_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_EW_L_G(action):
-#     if action == "EW_L_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_EW_L_Y(action):
-#     if action == "EW_L_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_WE_S_G(action):
-#     if action == "WE_S_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_WE_S_Y(action):
-#     if action == "WE_S_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_WE_SL_G(action):
-#     if action == "WE_SL_G":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_WE_SL_Y(action):
-#     if action == "WE_SL_Y":
-#         return True
-#     else:
-#         return False
-
-# def intendedActionIs_WE_L_G(action):
-#     if action == "WE_L_G":
-#         return True
-#     else:
-#         return False
-# def intendedActionIs_WE_L_Y(action):
-#     if action == "WE_L_Y":
-#         return True
-#     else:
-#         return False
-#---------------------------------- end ---------------------------------------#
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from typing import List
+    from Intention import Intention
+    from AgentPool import AgentPool
 
 
 #------------------- timeSinceLastCommunication predicates --------------------#
-def timeSinceCommunication_0(timeSinceCommunication):
-    if timeSinceCommunication == 0:
-        return True
-    else:
-        return False
+def timeSinceCommunication_0(timeSinceCommunication: int):
+    return timeSinceCommunication == 0
 
 
-def timeSinceCommunication_0_5(timeSinceCommunication):
-    if 0 < timeSinceCommunication < 5:
-        return True
-    else:
-        return False
+def timeSinceCommunication_0_5(timeSinceCommunication: int):
+    return 0 < timeSinceCommunication < 5
 
 
-def timeSinceCommunication_5_10(timeSinceCommunication):
-    if 5 < timeSinceCommunication < 10:
-        return True
-    else:
-        return False
+def timeSinceCommunication_5_10(timeSinceCommunication: int):
+    return 5 < timeSinceCommunication < 10
 
 
-def timeSinceCommunication_10_15(timeSinceCommunication):
-    if 10 < timeSinceCommunication < 15:
-        return True
-    else:
-        return False
+def timeSinceCommunication_10_15(timeSinceCommunication: int):
+    return 10 < timeSinceCommunication < 15
 
 
-def timeSinceCommunication_15_20(timeSinceCommunication):
-    if 15 < timeSinceCommunication < 20:
-        return True
-    else:
-        return False
+def timeSinceCommunication_15_20(timeSinceCommunication: int):
+    return 15 < timeSinceCommunication < 20
 
 
-def timeSinceCommunication_20_25(timeSinceCommunication):
-    if 20 < timeSinceCommunication < 25:
-        return True
-    else:
-        return False
+def timeSinceCommunication_20_25(timeSinceCommunication: int):
+    return 20 < timeSinceCommunication < 25
 #---------------------------------- end ----------------------------------#
 
 
 # EVALUATES VALIDITY OF A CUSTOM PREDICATE RELATIVE TO A COMMUNICATED INTENTION
-def customPredicate(predicate, intention):
+def customPredicate(predicate: str, intention: Intention) -> bool:
     predicate = predicate.split("_", 1)
 
-    if predicate[0] == intention.getTrafficLight().getName() and predicate[1] == intention.getAction():
-        return True
-    else:
-        return False
+    return predicate[0] == intention.getTrafficLight().getName() and predicate[1] == intention.getAction()
 
 
 # RETURN LIST OF PREDICATE FUNCTIONS AS DEFINED ABOVE
-def getPredicateSet(agentPool):
-    thisModule = sys.modules[__name__]  # Get reference to this module for next operation
-    methodsDict = dict(inspect.getmembers(thisModule, predicate=inspect.isfunction))  # Get a dictionary with all methods (predicates) in this module
+def getPredicateSet(agentPool: AgentPool):
+    # Get reference to this module for next operation
+    thisModule = sys.modules[__name__]
+    # Get a dictionary with all methods (predicates) in this module
+    methodsDict = dict(inspect.getmembers(thisModule, predicate=inspect.isfunction))
     # Remove all methods that are not predicates from dictionary
     methodsDict.pop("getPredicateSet")
     methodsDict.pop("getRandomPredicate")
     methodsDict.pop("getPredicateSetFromFile")
     methodsDict.pop("getAgentSpecificPredicates")
-    # methodsDict.pop("run")
 
     # Seperate methods/predicates from rest of data in dictionary into a list
-    predicateList = []
+    predicateList: List[str] = []
     for predicate in methodsDict:
         predicateList.append(predicate)
 
@@ -274,8 +67,8 @@ def getPredicateSet(agentPool):
 
 
 # RETURN LIST OF PREDICATE FUNCTIONS FROM AN INPUT FILE
-def getPredicateSetFromFile(file):
-    predicateList = []
+def getPredicateSetFromFile(file: str):
+    predicateList: List[str] = []
     f = open(file, "r")  # Open desired file
     for x in f:
         if "//" in x:
@@ -290,23 +83,16 @@ def getPredicateSetFromFile(file):
 
 
 # RETURN RANDOM PREDICATE FROM LIST OF PREDICATE FUNCTIONS
-def getRandomPredicate(agentPool):
-    # Add some ap specific stuff here
+def getRandomPredicate(agentPool: AgentPool):
     predicateList = getPredicateSet(agentPool)
     return predicateList[randrange(len(predicateList))]
 
 
-def getAgentSpecificPredicates(agentPool):
-    customPredicates = []
+def getAgentSpecificPredicates(agentPool: AgentPool):
+    customPredicates: List[str] = []
     for tl in agentPool.getAssignedTrafficLights():
         for partner in tl.getCommunicationPartners():
             for action in partner.getAgentPool().getActionSet():
                 pred = partner.getName() + "_" + action
                 customPredicates.append(pred)
     return customPredicates
-
-# def run():
-#      print("\nThe predicate list is:", getPredicateSetFromFile("predicatesForRSint.txt"))
-
-# if __name__ == "__main__":
-#     run()
