008f0039086eb3eba5ceafdaa038cd1679a34d55
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-01-31 12:48:30 -0700

    Cleaned up EVPredicateSet - minimized predicates - added type checking - removed unnneccessary imports and functions

--- EVPredicateSet.py
+++ EVPredicateSet.py
@@ -1,222 +1,162 @@
-import os
+from __future__ import annotations
+
 import sys
-import optparse
 import inspect
 from random import randrange
-from pprint import pprint
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from typing import List
+    from AgentPool import AgentPool
 
 
 #--------------------- EV Traffic Density ----------------------#
-def EVTrafficDensity_0(trafficDensity):
-    if trafficDensity == 0:
-        return True
-    else:
-        return False
+def EVTrafficDensity_0(trafficDensity: float):
+    return trafficDensity == 0
 
 
-def EVTrafficDensity_0_2(trafficDensity):
-    if 0 < trafficDensity <= 2:
-        return True
-    else:
-        return False
+def EVTrafficDensity_0_2(trafficDensity: float):
+    return 0 < trafficDensity <= 2
 
 
-def EVTrafficDensity_2_5(trafficDensity):
-    if 2 < trafficDensity <= 5:
-        return True
-    else:
-        return False
+def EVTrafficDensity_2_5(trafficDensity: float):
+    return 2 < trafficDensity <= 5
 
 
-def EVTrafficDensity_5_10(trafficDensity):
-    if 5 < trafficDensity <= 10:
-        return True
-    else:
-        return False
+def EVTrafficDensity_5_10(trafficDensity: float):
+    return 5 < trafficDensity <= 10
 
 
-def EVTrafficDensity_10_15(trafficDensity):
-    if 10 < trafficDensity <= 15:
-        return True
-    else:
-        return False
+def EVTrafficDensity_10_15(trafficDensity: float):
+    return 10 < trafficDensity <= 15
 
 
-def EVTrafficDensity_15_25(trafficDensity):
-    if 15 < trafficDensity <= 25:
-        return True
-    else:
-        return False
+def EVTrafficDensity_15_25(trafficDensity: float):
+    return 15 < trafficDensity <= 25
 
 
-def EVTrafficDensity_25_35(trafficDensity):
-    if 25 < trafficDensity <= 35:
-        return True
-    else:
-        return False
+def EVTrafficDensity_25_35(trafficDensity: float):
+    return 25 < trafficDensity <= 35
 
 
-def EVTrafficDensity_35_45(trafficDensity):
-    if 35 < trafficDensity <= 45:
-        return True
-    else:
-        return False
+def EVTrafficDensity_35_45(trafficDensity: float):
+    return 35 < trafficDensity <= 45
 
 
-def EVTrafficDensity_45_60(trafficDensity):
-    if 45 < trafficDensity <= 60:
-        return True
-    else:
-        return False
+def EVTrafficDensity_45_60(trafficDensity: float):
+    return 45 < trafficDensity <= 60
 
 
-def EVTrafficDensity_60_90(trafficDensity):
-    if 60 < trafficDensity <= 90:
-        return True
-    else:
-        return False
+def EVTrafficDensity_60_90(trafficDensity: float):
+    return 60 < trafficDensity <= 90
 
 
-def EVTrafficDensity_90_120(trafficDensity):
-    if 90 < trafficDensity <= 120:
-        return True
-    else:
-        return False
+def EVTrafficDensity_90_120(trafficDensity: float):
+    return 90 < trafficDensity <= 120
 
 
-def EVTrafficDensity_120_150(trafficDensity):
-    if 120 < trafficDensity <= 150:
-        return True
-    else:
-        return False
+def EVTrafficDensity_120_150(trafficDensity: float):
+    return 120 < trafficDensity <= 150
 
 
-def EVTrafficDensity_150(trafficDensity):
-    if trafficDensity > 150:
-        return True
-    else:
-        return False
+def EVTrafficDensity_150(trafficDensity: float):
+    return trafficDensity > 150
 #----------------------------- end -----------------------------#
 
 
 #----------------- EV distance to intersection ------------------#
-def EVDistanceToIntersection_0(distanceToIntersection):
-    if distanceToIntersection == 0:
-        return True
-    else:
-        return False
+def EVDistanceToIntersection_0(distanceToIntersection: float):
+    return distanceToIntersection == 0
 
 
-def EVDistanceToIntersection_0_50(distanceToIntersection):
-    if 0 < distanceToIntersection <= 50:
-        return True
-    else:
-        return False
+def EVDistanceToIntersection_0_50(distanceToIntersection: float):
+    return 0 < distanceToIntersection <= 50
 
 
-def EVDistanceToIntersection_50_100(distanceToIntersection):
-    if 50 < distanceToIntersection <= 100:
-        return True
-    else:
-        return False
+def EVDistanceToIntersection_50_100(distanceToIntersection: float):
+    return 50 < distanceToIntersection <= 100
 
 
-def EVDistanceToIntersection_100_200(distanceToIntersection):
-    if 100 < distanceToIntersection <= 200:
-        return True
-    else:
-        return False
+def EVDistanceToIntersection_100_200(distanceToIntersection: float):
+    return 100 < distanceToIntersection <= 200
 
 
-def EVDistanceToIntersection_200_300(distanceToIntersection):
-    if 200 < distanceToIntersection <= 300:
-        return True
-    else:
-        return False
+def EVDistanceToIntersection_200_300(distanceToIntersection: float):
+    return 200 < distanceToIntersection <= 300
 
 
-def EVDistanceToIntersection_400_500(distanceToIntersection):
-    if 400 < distanceToIntersection <= 500:
-        return True
-    else:
-        return False
+def EVDistanceToIntersection_400_500(distanceToIntersection: float):
+    return 400 < distanceToIntersection <= 500
 
 
-def EVDistanceToIntersection_500_600(distanceToIntersection):
-    if 500 < distanceToIntersection <= 600:
-        return True
-    else:
-        return False
+def EVDistanceToIntersection_500_600(distanceToIntersection: float):
+    return 500 < distanceToIntersection <= 600
 
 
-def EVDistanceToIntersection_600_800(distanceToIntersection):
-    if 600 < distanceToIntersection <= 800:
-        return True
-    else:
-        return False
+def EVDistanceToIntersection_600_800(distanceToIntersection: float):
+    return 600 < distanceToIntersection <= 800
 
 
-def EVDistanceToIntersection_800(distanceToIntersection):
-    if distanceToIntersection > 800:
-        return True
-    else:
-        return False
-#----------------------------- end -----------------------------#
+def EVDistanceToIntersection_800(distanceToIntersection: float):
+    return distanceToIntersection > 800
+    #----------------------------- end -----------------------------#
+
+    # EVALUATES VALIDITY OF LEADING EV LANE PREDICATE
 
 
-# TODO: will need to make individual predicates for each AP for the approaching EV's lane
-# #-------------------- EV Approach Direction --------------------#
-# def EVApproachingHorizontal(laneInfo):
-#     EVLane, horizontalLanes = laneInfo
-#     if EVLane is None:
-#         return False
-#     elif EVLane in horizontalLanes:
-#         return True
-#     else:
-#         return False
-
-
-# def EVApproachingVertical(laneInfo):
-#     EVLane, verticalLanes = laneInfo
-#     if EVLane is None:
-#         return False
-#     elif EVLane in verticalLanes:
-#         return True
-#     else:
-#         return False
-# #----------------------------- end -----------------------------#
+def lanePredicate(predicate: str, leadingEVLane: str):
+    return predicate.split("_", 1)[1] == leadingEVLane
 
 
 # RETURN LIST OF PREDICATE FUNCTIONS
-def getPredicateList():
+def getPredicateSet(agentPool: AgentPool, includeLanePredicates: bool = True):
     # Get reference to this module for next operation
     thisModule = sys.modules[__name__]
     # Get a dictionary with all methods (predicates) in this module
     methodsDict = dict(inspect.getmembers(thisModule, predicate=inspect.isfunction))
     # Remove all methods that are not predicates from dictionary
-    methodsDict.pop("getPredicateList")
+    methodsDict.pop("getPredicateSet")
     methodsDict.pop("getRandomPredicate")
-    methodsDict.pop("run")
-    methodsDict.pop("pprint")
+    methodsDict.pop("getAgentSpecificPredicates")
+    methodsDict.pop("lanePredicate")
+    methodsDict.pop("getRandomLanePredicate")
+    # methodsDict.pop("run")
 
     # Seperate methods/predicates from rest of data in dictionary into a list
-    predicateList = []
+    predicateList: List[str] = []
     for predicate in methodsDict:
         predicateList.append(predicate)
+
+    if includeLanePredicates:
+        predicateList += getAgentSpecificPredicates(agentPool)
 
     return predicateList
 
 
 # RETURN RANDOM PREDICATE FROM LIST OF PREDICATE FUNCTIONS
-def getRandomPredicate():
-    predicateList = getPredicateList()
+def getRandomPredicate(agentPool: AgentPool):
+    predicateList = getPredicateSet(agentPool, False)
     return (predicateList[randrange(len(predicateList))])
 
 
-def run():
-    print("\nThe predicate list is:")
-    pprint(getPredicateList())
+def getAgentSpecificPredicates(agentPool: AgentPool):
+    customPredicates: List[str] = []
+    for tl in agentPool.getAssignedTrafficLights():
+        for lane in tl.getLanes():
+            pred = f"leadingEVLane_{lane}"
+            customPredicates.append(pred)
+    return customPredicates
 
 
-if __name__ == "__main__":
-    run()
+def getRandomLanePredicate(agentPool):
+    lanePredicates = getAgentSpecificPredicates(agentPool)
+    return lanePredicates[randrange(len(lanePredicates))]
+
+
+# def run():
+#     print("The predicate list is:")
+#     pprint(getPredicateSet(False))
+
+
+# if __name__ == "__main__":
+#     run()
