fbadfedf3b637ac1a71a9b6770b48b8b28bc3fa7
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-02-11 20:19:13 -0700

    Implemented database.py

--- Database.py
+++ Database.py
@@ -1,5 +1,32 @@
+import requests
+
 
 class Database:
-    def __init__(self) -> None:
-        pass
-        self.url = "https://mlstatus.herokuapp.com/"
+    def __init__(self, startTime) -> None:
+        self.url = "https://mlstatus.herokuapp.com/database/"
+        self.startTime = startTime
+        self.generation = 0
+
+    def setOptions(self, options) -> None:
+        requests.post(self.url + "setOptions", json={
+            "startTime": self.startTime,
+            "options": options
+        })
+
+    def setGeneration(self, generation) -> None:
+        self.generation += generation
+
+    def updateAgentPool(self, id: str, agentPoolData: dict) -> None:
+        requests.post(self.url + "updateAgentPool", json={
+            "startTime": self.startTime,
+            "id": id,
+            "agentPoolData": agentPoolData,
+            "generation": self.generation
+        })
+
+    def storeOutput(self, outputData: dict):
+        requests.post(self.url + "storeOutput", json={
+            "startTime": self.startTime,
+            "outputData": outputData,
+            "generation": self.generation
+        })
