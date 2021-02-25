8cde4294c57820aaa031b6df8b8b0c54580aeb90
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-02-24 22:43:26 -0700

    Added number to EmergencyVehicle objects

--- EmergencyVehicle.py
+++ EmergencyVehicle.py
@@ -1,6 +1,7 @@
 class EmergencyVehicle:
     def __init__(self, ID: str = None, speed: float = None, distance: float = None, lane: str = None, queue: int = None, trafficDensity: float = None):
         self.ID: str = ID
+        self.number: int = int(ID.split("_")[0])
         self.speed: float = speed
         self.distance: float = distance
         self.lane: str = lane
@@ -11,13 +12,19 @@ class EmergencyVehicle:
         return f"ID: {self.ID}\n" + f"speed: {self.speed}\n" + f"distance: {self.distance}\n" + f"lane: {self.lane}\n" + f"queue: {self.queue}\n" + f"traffic density: {self.trafficDensity}\n"
 
     def __repr__(self) -> str:
-        return f"ID: {self.ID}\n" + f"speed: {self.speed}\n" + f"distance: {self.distance}\n" + f"lane: {self.lane}\n" + f"queue: {self.queue}\n" + f"traffic density: {self.trafficDensity}\n"
+        return str(self)
 
     def getID(self):
         return self.ID
 
     def setID(self, ID):
         self.ID = ID
+
+    def getNumber(self):
+        return self.number
+
+    def setNumber(self, number):
+        self.number = number
 
     def getSpeed(self):
         return self.speed
