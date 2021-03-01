24f971037c321a443c63083af69c2b1b0499e1bc
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-02-28 21:40:11 -0700

    Fix main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 from Simulation import Simulation
-from Tester import Tester
 
 
 Simulation().run()
-
-
-tester = Tester()
-
-tester.testRules("Thu_Feb_25_10:02:43_PM_2021", [True, False, True], gui=False)
