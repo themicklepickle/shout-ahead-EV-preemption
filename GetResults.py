6ca1046c5bd4875c53859aa7cdd08b75fada9700
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-04-26 23:34:37 -0600

    Jupyter notebook commands

--- GetResults.py
+++ GetResults.py
@@ -1,4 +1,5 @@
 ### SETUP ###
+# %%
 from API.Tester import Tester
 
 tester = Tester()
@@ -9,10 +10,12 @@ EV_layered_2 = "Sun_Apr_25_02:05:51_PM_2021"
 
 
 ### CONTROL ###
+# %%
 tester.testRules("blank", [0, 0, 1], False, 1)
 
 
 ### EV LAYERED #2 (FULL) ###
+# %%
 name = "EV_layered_2_51_average_EV_speed"
 tester.addBestPoolInGeneration(51, "averageEVSpeed", EV_layered_2, name)
 tester.addRuleSetToRuleSet(name, "base_system_59_average_EV_speed")
@@ -20,12 +23,14 @@ tester.testRules(name, [0, 0, 1], False, 1000, True)
 
 
 ### BASE SYSTEM (FULL) ###
+# %%
 name = "base_system_59_average_EV_speed"
 tester.addBestPoolInGeneration(59, "averageEVSpeed", base_system, name)
 tester.testRules(name, [0, 0, 1], False, 1000, True)
 
 
 ### EV LAYERED #1 (FULL) ###
+# %%
 name = "EV_layered_1_58_average_EV_speed"
 tester.addBestPoolInGeneration(58, "averageEVSpeed", EV_layered_1, name)
 tester.addRuleSetToRuleSet(name, "base_system_59_average_EV_speed")
@@ -33,6 +38,7 @@ tester.testRules(name, [0, 0, 1], False, 1000, True)
 
 
 ### EV LAYERED #2 (NO COOP) ###
+# %%
 name = "EV_layered_2_51_average_EV_speed"
 tester.addBestPoolInGeneration(51, "averageEVSpeed", EV_layered_2, name)
 tester.addRuleSetToRuleSet(name, "base_system_59_average_EV_speed")
@@ -40,6 +46,7 @@ tester.removeOneRuleSet(name, "RSint")
 tester.testRules(name, [0, 0, 1], False, 1000, True)
 
 ### BASE SYSTEM (NO COOP) ###
+# %%
 name = "base_system_59_average_EV_speed"
 tester.addBestPoolInGeneration(59, "averageEVSpeed", base_system, name)
 tester.removeOneRuleSet(name, "RSint")
@@ -47,6 +54,7 @@ tester.testRules(name, [0, 0, 1], False, 1000, True)
 
 
 ### EV LAYERED #1 (NO COOP) ###
+# %%
 name = "EV_layered_1_58_average_EV_speed"
 tester.addBestPoolInGeneration(58, "averageEVSpeed", EV_layered_1, name)
 tester.addRuleSetToRuleSet(name, "base_system_59_average_EV_speed")
