b9c798203d197d9340b4b7ce9558c804959981f0
Michael Xu <61663933+themicklepickle@users.noreply.github.com>
2021-04-26 23:28:42 -0600

    Test no coop

--- GetResults.py
+++ GetResults.py
@@ -12,18 +12,43 @@ EV_layered_2 = "Sun_Apr_25_02:05:51_PM_2021"
 tester.testRules("blank", [0, 0, 1], False, 1)
 
 
-### EV LAYERED #2 ###
-tester.addBestPoolInGeneration(51, "averageEVSpeed", EV_layered_2, "EV_layered_2_51_average_EV_speed")
-tester.addRuleSetToRuleSet("EV_layered_2_51_average_EV_speed", "base_system_59_average_EV_speed")
-tester.testRules("EV_layered_2_51_average_EV_speed", [0, 0, 1], False, 1000, True)
+### EV LAYERED #2 (FULL) ###
+name = "EV_layered_2_51_average_EV_speed"
+tester.addBestPoolInGeneration(51, "averageEVSpeed", EV_layered_2, name)
+tester.addRuleSetToRuleSet(name, "base_system_59_average_EV_speed")
+tester.testRules(name, [0, 0, 1], False, 1000, True)
 
 
-### BASE SYSTEM ###
-tester.addBestPoolInGeneration(59, "averageEVSpeed", base_system, "base_system_59_average_EV_speed")
-tester.testRules("base_system_59_average_EV_speed", [0, 0, 1], False, 1000, True)
+### BASE SYSTEM (FULL) ###
+name = "base_system_59_average_EV_speed"
+tester.addBestPoolInGeneration(59, "averageEVSpeed", base_system, name)
+tester.testRules(name, [0, 0, 1], False, 1000, True)
 
 
-### EV LAYERED #1 ###
-tester.addBestPoolInGeneration(58, "averageEVSpeed", EV_layered_1, "EV_layered_1_58_average_EV_speed")
-tester.addRuleSetToRuleSet("EV_layered_1_58_average_EV_speed", "base_system_59_average_EV_speed")
-tester.testRules("EV_layered_1_58_average_EV_speed", [0, 0, 1], False, 1000, True)
+### EV LAYERED #1 (FULL) ###
+name = "EV_layered_1_58_average_EV_speed"
+tester.addBestPoolInGeneration(58, "averageEVSpeed", EV_layered_1, name)
+tester.addRuleSetToRuleSet(name, "base_system_59_average_EV_speed")
+tester.testRules(name, [0, 0, 1], False, 1000, True)
+
+
+### EV LAYERED #2 (NO COOP) ###
+name = "EV_layered_2_51_average_EV_speed"
+tester.addBestPoolInGeneration(51, "averageEVSpeed", EV_layered_2, name)
+tester.addRuleSetToRuleSet(name, "base_system_59_average_EV_speed")
+tester.removeOneRuleSet(name, "RSint")
+tester.testRules(name, [0, 0, 1], False, 1000, True)
+
+### BASE SYSTEM (NO COOP) ###
+name = "base_system_59_average_EV_speed"
+tester.addBestPoolInGeneration(59, "averageEVSpeed", base_system, name)
+tester.removeOneRuleSet(name, "RSint")
+tester.testRules(name, [0, 0, 1], False, 1000, True)
+
+
+### EV LAYERED #1 (NO COOP) ###
+name = "EV_layered_1_58_average_EV_speed"
+tester.addBestPoolInGeneration(58, "averageEVSpeed", EV_layered_1, name)
+tester.addRuleSetToRuleSet(name, "base_system_59_average_EV_speed")
+tester.removeOneRuleSet(name, "RSint")
+tester.testRules(name, [0, 0, 1], False, 1000, True)
